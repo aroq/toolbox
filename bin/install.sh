8cee7f87e3a67546d12fd45e94fc2d2c9ae3cc17
Alexander Tolstikov <atolstikov@adyax.com>
2019-08-06 21:52:31 +0300

    Update install procedure

--- bin/install.sh
+++ bin/install.sh
@@ -5,6 +5,8 @@
 export TOOLBOX_ORG=${1:-aroq}
 export TOOLBOX_PROJECT=${2:-toolbox}
 export TOOLBOX_BRANCH=${3:-master}
+export TOOLBOX_DIR=${4:-.toolbox/core}
+
 export GITHUB_REPO="https://github.com/${TOOLBOX_ORG}/${TOOLBOX_PROJECT}.git"
 
 if [ "$TOOLBOX_PROJECT" ] && [ -d "$TOOLBOX_PROJECT" ]; then
@@ -12,5 +14,7 @@ if [ "$TOOLBOX_PROJECT" ] && [ -d "$TOOLBOX_PROJECT" ]; then
   rm -rf "$TOOLBOX_PROJECT"
 fi
 
-echo "Cloning ${GITHUB_REPO}#${TOOLBOX_BRANCH}..."
-git clone -b "$TOOLBOX_BRANCH $GITHUB_REPO"
+mkdir -p "${TOOLBOX_DIR}"
+
+echo "Cloning ${GITHUB_REPO}#${TOOLBOX_BRANCH} into ${TOOLBOX_DIR}..."
+git clone -b "${TOOLBOX_BRANCH} ${GITHUB_REPO} ${TOOLBOX_DIR}"
