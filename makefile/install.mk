f1ca2239c4d62a0e3bb4347cabb7e898370d430b
Alexander Tolstikov <atolstikov@adyax.com>
2019-08-12 17:33:38 +0300

    Add toolbox cmd

--- makefile/install.mk
+++ makefile/install.mk
@@ -6,7 +6,7 @@ export TOOLBOX_DIR ?= .toolbox
 export TOOLBOX_CORE_DIR ?= $(TOOLBOX_DIR)/core
 export TOOLBOX_PATH ?= $(shell until [ -d "$(TOOLBOX_DIR)" ] || [ "`pwd`" == '/' ]; do cd ..; done; pwd)/$(TOOLBOX_DIR)
 
-_TOOLBOX_CORE_TOOLS_DEPS_INIT_IMAGE ?= aroq/toolbox
+_TOOLBOX_CORE_TOOLS_TOOLBOX_IMAGE ?= aroq/toolbox
 
 -include $(TOOLBOX_PATH)/core/makefile/Makefile
 
@@ -15,7 +15,7 @@ _TOOLBOX_CORE_TOOLS_DEPS_INIT_IMAGE ?= aroq/toolbox
 init::
 	@curl -H 'Cache-Control: no-cache' --retry 5 --fail --silent --retry-delay 1 https://raw.githubusercontent.com/$(TOOLBOX_ORG)/$(TOOLBOX_PROJECT)/$(TOOLBOX_BRANCH)/bin/install.sh?$(date +%s) | \
 		bash -s "$(TOOLBOX_ORG)" "$(TOOLBOX_PROJECT)" "$(TOOLBOX_BRANCH)" "$(TOOLBOX_DIR)"
-	@$(MAKE) .toolbox/core/tools/deps/init
+	@$(MAKE) .toolbox/core/tools/toolbox deps install
 
 .PHONY : clean
 ## Clean toolbox
